1f037d3bd15c09ce62af316b217cbe88bb39f09b
Bardiya Basafa <Bardiya.Basafa@gmail.com>
2026-06-24 10:45:19 +0330

    dijstra routing completed

--- main.cpp
+++ main.cpp
@@ -1,119 +1,173 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <sqlite3.h>
+// #include <sqlite3.h>
 #include <filesystem>
 #include <queue>
 
 using namespace std;
 using pii = pair<int, int>;
 
-struct node
-{
-    int u;
-    double delay;
-};
-
 struct edge
 {
-    node u;
-    node v;
+    int u;
+    int v;
     double d;
     double t;
     double w;
     double total_cost()
     {
-        return d * t * w + v.delay;
+        // return d * t * w;
+        return d;
     }
 };
 
-void getFromDb(vector<node> &nodes, vector<edge> &edges)
+// void getFromDb(vector<int> &nodes, vector<double> &delays, vector<edge> &edges)
+// {
+
+//     const string dbPath = "graph.db";
+
+//     cout << "Database location: "
+//          << filesystem::absolute(dbPath) << endl;
+
+//     sqlite3 *db = nullptr;
+//     int rc = sqlite3_open(dbPath.c_str(), &db);
+
+//     if (rc != SQLITE_OK)
+//     {
+//         cerr << "Cannot open database: " << sqlite3_errmsg(db) << endl;
+//         return;
+//     }
+
+//     cout << "Database opened successfully!" << endl;
+
+//     sqlite3_stmt *stmt_nodes;
+
+//     string sql_nodes = "SELECT id, delay FROM nodes";
+
+//     sqlite3_prepare_v2(db, sql_nodes.c_str(), -1, &stmt_nodes, NULL);
+
+//     while (sqlite3_step(stmt_nodes) == SQLITE_ROW)
+//     {
+
+//         int u = sqlite3_column_int(stmt_nodes, 0);
+//         double delay = sqlite3_column_double(stmt_nodes, 1);
+
+//         nodes.push_back(u - 1);
+//         delays.push_back(delay);
+//     }
+
+//     sqlite3_finalize(stmt_nodes);
+
+//     sqlite3_stmt *stmt_edges;
+
+//     string sql_edges = "SELECT source, target, distance, trafic, weather FROM edges";
+//     sqlite3_prepare_v2(db, sql_edges.c_str(), -1, &stmt_edges, NULL);
+
+//     while (sqlite3_step(stmt_edges) == SQLITE_ROW)
+//     {
+//         int u = sqlite3_column_int(stmt_edges, 1);
+
+//         int v = sqlite3_column_int(stmt_edges, 2);
+
+//         double d = sqlite3_column_double(stmt_edges, 3);
+
+//         double trafic = sqlite3_column_double(stmt_edges, 4);
+
+//         double weather = sqlite3_column_double(stmt_edges, 5);
+
+//         edge e = {u - 1, v - 1, d, trafic, weather};
+//         edge erev = {v - 1, u - 1, d, trafic, weather};
+//         edges.push_back(e);
+//         edges.push_back(erev);
+//     }
+
+//     sqlite3_finalize(stmt_edges);
+
+//     // cout << nodes.size() << endl;
+//     // cout << edges.size() << endl;
+// }
+
+vector<double> Dijstra(vector<vector<edge>> &graph, int source, vector<int> &parent, vector<double> &delays)
 {
-
-    const string dbPath = "graph.db";
-
-    cout << "Database location: "
-         << filesystem::absolute(dbPath) << endl;
-
-    sqlite3 *db = nullptr;
-    int rc = sqlite3_open(dbPath.c_str(), &db);
-
-    if (rc != SQLITE_OK)
-    {
-        cerr << "Cannot open database: " << sqlite3_errmsg(db) << endl;
-        return;
-    }
-
-    cout << "Database opened successfully!" << endl;
-
-    sqlite3_stmt *stmt_nodes;
-
-    string sql_nodes = "SELECT id, delay FROM nodes";
-
-    sqlite3_prepare_v2(db, sql_nodes.c_str(), -1, &stmt_nodes, NULL);
-
-    while (sqlite3_step(stmt_nodes) == SQLITE_ROW)
-    {
-        node n;
-
-        n.u = sqlite3_column_int(stmt_nodes, 0);
-        n.delay = sqlite3_column_double(stmt_nodes, 1);
-
-        nodes.push_back(n);
-    }
-
-    sqlite3_finalize(stmt_nodes);
-
-    sqlite3_stmt *stmt_edges;
-
-    string sql_edges = "SELECT source, target, distance, trafic, weather FROM edges";
-    sqlite3_prepare_v2(db, sql_edges.c_str(), -1, &stmt_edges, NULL);
-
-    while (sqlite3_step(stmt_edges) == SQLITE_ROW)
-    {
-        int u = sqlite3_column_int(stmt_edges, 1);
-
-        int v = sqlite3_column_int(stmt_edges, 2);
-
-        double d = sqlite3_column_double(stmt_edges, 3);
-
-        double trafic = sqlite3_column_double(stmt_edges, 4);
-
-        double weather = sqlite3_column_double(stmt_edges, 5);
-
-        edge e = {nodes[u - 1], nodes[v - 1], d, trafic, weather};
-        edges.push_back(e);
-    }
-
-    // cout << nodes.size() << endl;
-    // cout << edges.size() << endl;
-}
-
-void Dijstra(vector<edge> &graph, node source)
-{
-    vector<int> dist(graph.size(), __INT_MAX__);
-    vector<int> parent(graph.size(), -1);
-    int s = source.u - 1;
+    vector<double> dist(graph.size(), __INT_MAX__);
+    int s = source;
     dist[s] = 0;
     priority_queue<pii, vector<pii>, greater<pii>> pq;
-    pq.push(make_pair(0, s));
+    pq.emplace(make_pair(0, s));
     while (!pq.empty())
     {
-        auto [du, u] = pq.top();
-        if (du!=dist[u])
+        pii p = pq.top();
+        pq.pop();
+        int u = p.second;
+        int d = p.first;
+        if (d > dist[u])
         {
-            
+            continue;
         }
-        
+
+        for (auto &&e : graph[u])
+        {
+            int v = e.v;
+            int w = e.total_cost() + delays[v];
+
+            if (dist[u] + w < dist[v])
+            {
+                parent[v] = u;
+                dist[v] = dist[u] + w;
+                pq.emplace(make_pair(dist[v], v));
+            }
+        }
     }
+
+    return dist;
 }
 
 int main(int argc, char const *argv[])
 {
 
-    vector<node> nodes;
-    vector<edge> edges;
-    getFromDb(nodes, edges);
+    vector<int> nodes = {0, 1, 2, 3, 4};
+    vector<double> delays = {0, 0, 0, 0, 0};
+    vector<edge> edges = {
+        {0, 1, 4, 0, 0},
+        {0, 2, 8, 0, 0},
+        {1, 2, 3, 0, 0},
+        {1, 4, 6, 0, 0},
+        {4, 3, 10, 0, 0},
+        {2, 3, 2, 0, 0},
+    };
+
+    cout << edges.size() << endl;
+
+    // complete graph
+    vector<vector<edge>> graph(nodes.size());
+
+    for (auto &&u : nodes)
+    {
+        for (auto &&edg : edges)
+        {
+            if (edg.u == u)
+            {
+                graph[u].push_back(edg);
+            }
+            else if (edg.v == u)
+            {
+                edge new_edg = edg;
+                swap(new_edg.u, new_edg.v);
+                graph[u].push_back(new_edg);
+            }
+        }
+    }
+
+    vector<int> parent(graph.size());
+    auto dist = Dijstra(graph, 0, parent, delays);
+
+    for (auto &&i : parent)
+    {
+        cout << i << " ";
+    }
+
+    // getFromDb(nodes, delays, edges);
 
     return 0;
 }
